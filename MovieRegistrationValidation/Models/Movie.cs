0c93ef1ee67ce2d54a37a661e3ecc16020b10e4e
Nate <nsdavis@mtu.edu>
2021-02-18 17:25:53 -0500

    Completed Lab Req

---
+++ MovieRegistrationValidation/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistrationValidation.Models
+{
+    public enum GenreType
+    {
+        Action,
+        Comedy,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        Thriller
+    }
+    public class Movie
+    {
+        public int ID { get; set; } = 1;
+        public string Title { get; set; } = "";
+        public GenreType Genre { get; set; }
+        public int Year { get; set; }
+        public string Actors { get; set; } = "";
+        public string Directors { get; set; } = "";
+    }
+}
